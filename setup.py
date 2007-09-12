cb451c1d8cdb2e21af7fac3846180a5be9ea61bd
Christian Zagrodnick <cz@gocept.com>
2007-09-12 12:50:26 +0000

    reading in the readme.txt, next version is 1.0b2

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = "z3c.flashmessage",
-    version = "1.0b1",
+    version = "1.0b2",
     author = "Jasper Op de Coul, Christian Theune",
     author_email = "jasper@infrae.com, ct@gocept.com",
     description = "A package for sending `flash messages` to users.",
+    long_description=file(os.path.join(
+        os.path.dirname(__file__), 'src', 'z3c', 'flashmessage',
+        'README.txt')).read(),
     license = "ZPL 2.1",
     keywords = "zope3",
     zip_safe=False,
