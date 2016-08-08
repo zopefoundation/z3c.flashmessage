c2beea786bc7aa05244b463270db82f78855f63b
Michael Howitz <mh@gocept.com>
2016-08-08 17:03:01 +0200

    Back to development: 2.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ TEST_REQUIREMENTS = [
 
 setup(
     name="z3c.flashmessage",
-    version='2.0',
+    version='2.1.dev0',
     author="Jasper Op de Coul, Christian Theune",
     author_email="jasper@infrae.com, mail@gocept.com",
     description="A package for sending `flash messages` to users.",
